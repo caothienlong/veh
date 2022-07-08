d5d44ccd945ef817ab3a3efd5ec7b443d8f83c67
root <root@vps-zap941681-1.zap-srv.com>
2022-07-08 18:18:09 +0000

    Uploaded car mods for Mysterious city

---
+++ [police]/taurus/__resource.lua
@@ -0,0 +1,29 @@
+--[[
+
+Made by Dunning
+Do not Distribute
+
+]]--
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+	'vehicles.meta',
+	'carcols.meta',
+	'carvariations.meta',
+	'handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+
+
+
+-- client_scripts {
+-- 	'data/vehnames.lua',
+-- 	'stream/Vehicles/14tah/mods/14tah.lua',
+-- 	'stream/Vehicles/11cvpi/mods/11cvpi.lua',
+-- }
+
